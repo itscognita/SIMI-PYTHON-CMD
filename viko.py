23516bcd66a2892af0630779a18a1431bb16e2a0
Viko Dwi Kurniawan <85740085+vikodk67@users.noreply.github.com>
2021-08-28 13:17:37 +0700

    Add files via upload
    jalankan pake cmd:V atau termos

---
+++ viko.py
@@ -0,0 +1,16 @@
+# BY VIKO
+# pip install requests
+# pip install colorama
+
+import requests
+from colorama import init, Fore, Back, Style
+init(autoreset=True)
+
+chate = raw_input("Kirim pesan kesimi : ")
+
+response = requests.get('https://viko-api.herokuapp.com/api/f/simi?apikey=rxking&query=' + chate)
+viko = response
+if viko == "i":
+        continue
+    print(val)
+print(Back.YELLOW + Fore.YELLOW + viko.json()["result"])
